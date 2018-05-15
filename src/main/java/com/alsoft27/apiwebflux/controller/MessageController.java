cdf816adb528b33bcd2199162a3bad765fcaf3b9
alsoft27 <alsoft27@gmail.com>
2018-05-15 11:34:30 +0200

    thymeleaf other form

--- src/main/java/com/alsoft27/apiwebflux/controller/MessageController.java
+++ src/main/java/com/alsoft27/apiwebflux/controller/MessageController.java
@@ -1,14 +1,18 @@
 package com.alsoft27.apiwebflux.controller;
 
+import java.time.Duration;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
 import com.alsoft27.apiwebflux.document.Message;
 import com.alsoft27.apiwebflux.repository.MessageRepository;
@@ -36,6 +40,13 @@ public class MessageController {
 	@GetMapping(value = "/stream/messages", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public Flux<Message> streamAllMessages() {
 		return messageRepository.findAll();
+	}
+
+	@GetMapping("/messages2")
+	public String listMessages(Model model) {
+		Flux<Message> flux = messageRepository.findAll().delayElements(Duration.ofSeconds(1));
+		model.addAttribute("messages", new ReactiveDataDriverContextVariable(flux, 1));
+		return "messages2";
 	}
 
 	@GetMapping("/")
